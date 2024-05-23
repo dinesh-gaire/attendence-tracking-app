b66066a516aef37c53d51a06e778c86d7ff01d6a
dinesh-gaire <132206290+dinesh-gaire@users.noreply.github.com>
2024-05-23 05:13:59 +0000

    dark theme functionality added

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,15 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
+import { useTheme } from 'next-themes'
 
 const Dashboard = () => {
+  const { setTheme } = useTheme()
+
+  useEffect(() => {
+    setTheme('system')
+  }, [])
+  
   return (
     <div>Dashboard</div>
   )
