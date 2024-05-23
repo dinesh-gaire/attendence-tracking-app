b0651abe4d392d77d3386811b1053f02fbe073a7
dinesh-gaire <132206290+dinesh-gaire@users.noreply.github.com>
2024-05-23 01:38:10 +0000

    dashboard page added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  useEffect(()=>{
+    redirect('/api/auth/login?post_login_redirect_url=/dashboard')
+  },[])
   return (
     <div>
-      <h2>Welcome</h2>
-      <Button>Click</Button>
+      <h1>Welcome</h1>
     </div>
   );
 }
