73c8e298149948ba8951622c50356c78706e1496
dinesh-gaire <132206290+dinesh-gaire@users.noreply.github.com>
2024-05-28 17:36:05 +0000

    cards added in dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,17 +1,45 @@
 "use client"
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTheme } from 'next-themes'
+import MonthSelection from '../_components/MonthSelection'
+import GradeSelect from '../_components/GradeSelect'
+import GlobalApi from '../_services/GlobalApi'
+import moment from 'moment'
+import StatusList from './_components/StatusList'
 
 const Dashboard = () => {
   const { setTheme } = useTheme()
+  const [selectedMonth, setSelectedMonth] = useState();
+  const [selectedGrade, setSelectedGrade] = useState();
+  const [attendanceList, setAttendanceList] = useState();
 
   useEffect(() => {
-    setTheme('light')
-  }, [])
+    // setTheme('light')
+    getStudentAttendance()
+  }, [selectedMonth||selectedGrade])
+
+  //used to get student attendance for given month and date
+  const getStudentAttendance=()=>{
+    GlobalApi.GetAttendanceList(selectedGrade,moment(selectedMonth).format('MM/yyyy'))
+    .then(resp=>{
+      // console.log(resp);
+      setAttendanceList(resp.data)
+    })
+  }
   
   return (
-    <div>Dashboard</div>
+    <div className='p-10'>
+      <div className='flex items-center justify-between'>
+        <h2 className='font-bold text-2xl'>Dashboard</h2>
+
+        <div className='flex items-center gap-4'>
+          <MonthSelection selectedMonth={setSelectedMonth}/>
+          <GradeSelect selectedGrade={setSelectedGrade}/>
+        </div>
+      </div>
+      <StatusList attendanceList={attendanceList}/>
+    </div>
   )
 }
 
